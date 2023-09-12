1aa1f2b062a74bb98fb43dd5413fc58cf96c18f2
Qewertyy <99824993+Qewertyy@users.noreply.github.com>
2023-09-12 13:50:03 +0000

    minor changes

--- plugins/start.py
+++ plugins/start.py
@@ -3,6 +3,15 @@
 from pyrogram import Client, filters, types as t
 from bot import StartTime
 
-@Client.on_message(filters.command("start"))
+@Client.on_message(filters.command(["start","help","repo"]))
 async def start(_: Client, m: t.Message):
-    await m.reply_text("Just an image generation bot for free by @Qewertyy.")
+    await m.reply_text(
+        "Just an image generation bot by @Qewertyy.\n\nCommands: \n`/draw`: create images\n`/upscale`: upscale your images",
+        reply_markup=t.InlineKeyboardMarkup(
+            [
+                [
+                    t.InlineKeyboardButton(text="Source",url="https://github.com/Qewertyy/SDWaifuRobot")
+                ]
+            ]
+        )
+    )
