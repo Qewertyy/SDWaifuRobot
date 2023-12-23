b08dc7d09a550b6f39da474b4928121aa8ed80ef
Qewertyy <99824993+Qewertyy@users.noreply.github.com>
2023-12-23 06:35:08 +0000

    minor changes

--- plugins/images.py
+++ plugins/images.py
@@ -13,12 +13,11 @@ async def searchImages(_: Client,m:t.Message):
             return await reply.edit("What do you want to search?")
         output = await SearchImages(prompt,"google")
         if output['code'] != 2:
-            await reply.delete()
             return await reply.edit("Ran into an error.")
-        images = random.choices(output['content'],k=8)
+        images = output['content']
         if len(images) == 0:
-            await reply.delete()
             return await reply.edit("No results found.")
+        images = random.choices(images,k=8 if len(images) > 8 else len(images))
         media = []
         for image in images:
             content = getImageContent(image['imageUrl'])
@@ -27,10 +26,9 @@ async def searchImages(_: Client,m:t.Message):
                 continue
             else:
                 media.append(t.InputMediaPhoto(io.BytesIO(content)))
-        await _.send_media_group(
-            m.chat.id,
+        await m.reply_media_group(
             media,
-            reply_to_message_id=m.id
+            quote=True
             )
         await reply.delete()
     except (errors.ExternalUrlInvalid, errors.WebpageCurlFailed,errors.WebpageMediaEmpty) as e:
